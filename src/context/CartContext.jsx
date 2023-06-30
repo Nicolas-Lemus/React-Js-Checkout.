df2f0e8a81369927ec6bb0c5224a86343a58ebb6
Nicolas Lemus <nicolaswaldemarlemuscurbelo@gmail.com>
2023-06-29 21:34:13 -0300

    Destructuramos en CartContext {} estaba en []

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,7 +3,8 @@ import React, { useState, createContext } from 'react';
 export const CardContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [count, setCount] = useState({ qtyItems: 0, Tecnologia: [] });
+  //cambie de [] a {} y ahora me aparece la pagina de productos e demas 21:33hs.
+  const {count, setCount} = useState({ qtyItems: 0, Tecnologia: [] });
 
   return (
     <CardContext.Provider value={{count, setCount}}>
